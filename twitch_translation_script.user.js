abfddaa3ffb561c81e66811547126aa053b377a5
Magof <95078143+magofera@users.noreply.github.com>
2023-09-19 05:46:31 -0300

    Update twitch_translation_script.user.js

--- twitch_translation_script.user.js
+++ twitch_translation_script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch.tv chat Translation
-// @namespace    https://github.com/jeanmoraes0/twitch-translation-script
+// @namespace    https://github.com/magofera/twitch-translation-script
 // @version      2.0
 // @description  Add a button to the Twitch.tv website that opens a menu to translate messages to the Twitch.tv chat.
 // @author       Jean Moraes
